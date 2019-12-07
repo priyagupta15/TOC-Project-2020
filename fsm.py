4a4bb59fb03161d30c8024e08eac327c7ea6ee3c
priyagupta <priyagupta1511@gmail.com>
2019-12-07 14:35:43 +0800

    TOC Code starts

--- fsm.py
+++ fsm.py
@@ -1,6 +1,7 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message
+
+from utils import *
 
 
 class TocMachine(GraphMachine):
@@ -15,6 +16,14 @@ class TocMachine(GraphMachine):
         text = event.message.text
         return text.lower() == "go to state2"
 
+    def is_going_to_state3(self, event):
+        text = event.message.text
+        return text.lower() == "go to state3"
+
+
+
+
+
     def on_enter_state1(self, event):
         print("I'm entering state1")
 
@@ -25,12 +34,36 @@ class TocMachine(GraphMachine):
     def on_exit_state1(self):
         print("Leaving state1")
 
+
+
+
+
+
     def on_enter_state2(self, event):
         print("I'm entering state2")
 
         reply_token = event.reply_token
         send_text_message(reply_token, "Trigger state2")
+        send_image_url(reply_token,"https://i.imgur.com/jJKYpDr.jpg")
         self.go_back()
 
-    def on_exit_state2(self):
+    def on_exit_state2(self): 
         print("Leaving state2")
+
+
+
+
+
+
+
+
+    def on_enter_state3(self, event):
+        print("I'm entering state3")
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, "Trigger state3")
+        confirm_message(reply_token)
+        self.go_back()
+
+    def on_exit_state3(self):
+        print("Leaving state3")
--- utils.py
+++ utils.py
@@ -2,22 +2,44 @@ import os
 
 from linebot import LineBotApi, WebhookParser
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-
+from linebot.models import *
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
 
 
 def send_text_message(reply_token, text):
     line_bot_api = LineBotApi(channel_access_token)
-    line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
+    message = TextSendMessage(text=text)
+    line_bot_api.reply_message(reply_token, message)
 
-    return "OK"
 
 
-"""
-def send_image_url(id, img_url):
-    pass
+def send_image_url(reply_token,img_url):
+    line_bot_api = LineBotApi(channel_access_token)  
 
-def send_button_message(id, text, buttons):
-    pass
-"""
+    message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
+
+    line_bot_api.reply_message(reply_token,message)
+
+
+
+def confirm_message(id,text,btn):
+    line_bot_api = LineBotApi(channel_access_token) 
+    
+    message = TemplateSendMessage(
+    alt_text='Confirm template',
+    template=ConfirmTemplate(
+        text='Are you sure?',
+        actions=[
+            PostbackTemplateAction(
+                label='postback',
+                text='postback text',
+                data='action=buy&itemid=1'
+            ),
+            MessageTemplateAction(
+                label='message',
+                text='message text') ]   ) )
+
+
+    line_bot_api.reply_message(reply_token, message)
+ 
