39640bbb185e2fc52d61aa091653d33a5f34d2ab
priyagupta <priyagupta1511@gmail.com>
2019-12-11 10:06:40 +0800

    done

--- app.py
+++ app.py
@@ -114,11 +114,13 @@ def callback():
             continue
         if not isinstance(event.message, TextMessage):
             continue
-
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text)
-        )
-
+        if not isinstance(event.message.text, str):
+            continue
+        print(f"\nFSM STATE: {machine.state}")
+        print(f"REQUEST BODY: \n{body}")
+        response = machine.advance(event)
+        if response == False:
+            send_text_message(event.reply_token, "Okay!")
     return "OK"
 
 
